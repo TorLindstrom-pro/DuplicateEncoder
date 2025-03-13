5f87e3d27e055b980c38d21de4d79baaac33475e
TorLindstrom <tor.lindstrom@winlas.se>
2025-03-13 13:37:51 +0100

    refactor: extract local function

--- DuplicateEncoder/Kata.cs
+++ DuplicateEncoder/Kata.cs
@@ -4,7 +4,10 @@ public class Kata
 {
 	public static string DuplicateEncode(string empty)
 	{
-		return empty.Aggregate("", (result, currentLetter) => 
-			result + (empty.Count(letter => letter == currentLetter) > 1 ? ")" : "("));
+		return empty.Aggregate("", (result, letter) => 
+			result + EncodeLetter(letter));
+
+		string EncodeLetter(char currentLetter) => 
+			empty.Count(letter => letter == currentLetter) > 1 ? ")" : "(";
 	}
 }
